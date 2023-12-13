4337c98b107ec20dbe0a00dfe6a4e6d5bcadbd84
itsMeVela <velaputri681@gmail.com>
2023-12-13 22:11:16 +0700

    cart.js

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems: [],
+}
+
+export const cartSlice = createSlice({
+   name: "cart", 
+   initialState,
+   reducers: {
+       addItemToCart: () => {},
+   },
+});
+
+export const{ addItemToCart } = cartSlice.actions;
